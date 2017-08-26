1f68292f3412bd542f0b329f6f1515ecc24d1a64
anamariaagudelo <anamariaagudeloisi@gmail.com>
2017-08-25 22:05:54 -0500

    Commit Heroes Renombrados

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,13 +2,13 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Chapulin' },
-  { id: 13, name: 'Bombastic' },
+  { id: 12, name: 'Capitan America' },
+  { id: 13, name: 'Babawoman' },
   { id: 14, name: 'Celeritas' },
   { id: 15, name: 'Magneta' },
-  { id: 16, name: 'ChocoKrispis' },
+  { id: 16, name: 'Flash' },
   { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
+  { id: 18, name: 'Miss Anny' },
   { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 20, name: 'Mujer Maravilla' }
 ];
